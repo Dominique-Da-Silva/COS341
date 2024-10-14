f57182b8a8fbd1f7dbf502ff43cb83d16d78699b
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-10-14 15:15:40 +0200

    23 states transitions

--- Project/parser.py
+++ Project/parser.py
@@ -68,33 +68,82 @@ class SLRParser:
         You need to fill this method with the actual parsing table entries based on your grammar.
         """
 
-        # Example of how to define the action table (to be replaced with your parsing table):
+        self.action_table = {
+            (0, 'MAIN'): ('shift', 1),
+            (1, 'NUM_TYPE'): ('shift', 4),
+            (1, 'TEXT_TYPE'): ('shift', 5),
+            (1, 'BEGIN'): ('reduce',1),
+            (2, 'BEGIN'): ('shift',7),
+            (3, 'VNAME'): ('shift',9),
+            (4, 'VNAME'): ('reduce',3),
+            (5, 'VNAME') : ('reduce',4),
+            (6, 'NUM_TYPE'): ('shift', 14),
+            (6, 'END'): ('reduce', 47),
+            (6, 'VOID'): ('shift', 15),
+            (6, 'EOF'): ('reduce', 47),
+            (7, 'VNAME'): ('shift', 9),
+            (7, 'END'): ('reduce', 7),
+            (7, 'SKIP'): ('shift', 18),
+            (7, 'HALT'): ('shift', 19), 
+            (7, 'PRINT'): ('shift', 20),
+            (7, 'RETURN'): ('shift', 24),
+            (7, 'IF'): ('shift', 27),
+            (7, 'FNAME'): ('shift', 28),
+            (8, 'COMMA'): ('shift', 29),
+            (9, 'COMMA'): ('reduce',5),
+            (9, 'SEMICOLON'): ('reduce',5),
+            (9, 'INPUT_OP'): ('reduce', 5),
+            (9, 'ASSIGN'): ('reduce', 5),
+            (9, 'RPAREN'): ('reduce', 5),
+            (10, 'EOF'): ('accept'),
+            (11, 'NUM_TYPE'): ('shift', 14),
+            (11, 'END'): ('reduce', 47),
+            (11, 'VOID'): ('shift', 15),
+            (11, 'EOF'): ('reduce',47),
+            (12, 'LBRACE'): ('shift', 33),
+            (13, 'FNAME'): ('shift', 28),
+            (14, 'FNAME'): ('reduce', 51),
+            (15, 'FNAME'): ('reduce', 52),
+            (16, 'END'): ('shift', 35),
+            (17, 'SEMICOLON'): ('shift', 36),
+            (18, 'SEMICOLON'): ('reduce', 9),
+            (19, 'SEMICOLON'): ('reduce', 10),
+            (20, 'VNAME'): ('shift', 9),
+            (20, 'CONST_N'): ('shift', 40),
+            (20, 'CONST_T'): ('shift', 41),
+            (21, 'SEMICOLON'): ('reduce', 12),
+            (22, 'SEMICOLON'): ('reduce', 13),
+            (23, 'SEMICOLON'): ('reduce', 14),
+        }
 
-        # The keys are tuples of (state, input_symbol)
-        # The values are actions: ('shift', next_state), ('reduce', production_number), or ('accept',)
-        # Terminals should match the 'token_class' from your lexer
 
-        # self.action_table = {
-        #     (0, 'id'): ('shift', 5),
-        #     (0, '('): ('shift', 4),
-        #     (1, '+'): ('shift', 6),
-        #     (1, 'EOF'): ('accept',),
-        #     # ... other entries ...
-        # }
-
-        # Example of how to define the goto table:
-
-        # The keys are tuples of (state, non_terminal)
-        # The values are the next state numbers
-
-        # self.goto_table = {
-        #     (0, 'E'): 1,
-        #     (0, 'T'): 2,
-        #     (0, 'F'): 3,
-        #     # ... other entries ...
-        # }
-
-        pass  # Remove this pass statement when you add the parsing table entries
+        self.goto_table = {
+            (1, 'GLOBVARS'): 2,
+            (1, 'VTYPE'): 3,
+            (2, 'ALGO'): 6,
+            (3, 'VNAME'): 8,
+            (6, 'FUNCTIONS'):10,
+            (6, 'DECL'): 11,
+            (6, 'HEADER'): 12,
+            (6, 'FTYP'): 13,
+            (7, 'VNAME'): 25,
+            (7, 'INSTRUC'): 16,
+            (7, 'COMMAND'): 17,
+            (7, 'ASSIGN'): 21,
+            (7, 'CALL'): 22,
+            (7, 'BRANCH'): 23,
+            (7, 'FNAME'): 26,
+            (11, 'FUNCTIONS'): 30,
+            (11, 'DECL'): 11,
+            (11, 'HEADER'): 12,
+            (11, 'FTYP'): 13,
+            (12, 'BODY'): 31,
+            (12, 'PROLOG'): 32,
+            (13, 'FNAME'): 34,
+            (20, 'VNAME'): 38,
+            (20, 'ATOMIC'): 37,
+            (20, 'CONST'): 39, 
+        }
 
 
     '''
@@ -106,19 +155,181 @@ class SLRParser:
         You need to fill this method with your grammar's production rules.
         """
 
-        # Example of how to define grammar rules:
-
-        # self.grammar_rules = [
-        #     ProductionRule('E\'', ['E']),          # Production 0
-        #     ProductionRule('E', ['E', '+', 'T']),  # Production 1
-        #     ProductionRule('E', ['T']),            # Production 2
-        #     ProductionRule('T', ['T', '*', 'F']),  # Production 3
-        #     ProductionRule('T', ['F']),            # Production 4
-        #     ProductionRule('F', ['(', 'E', ')']),  # Production 5
-        #     ProductionRule('F', ['id']),           # Production 6
-        # ]
-
-        pass  # Remove this pass statement when you add the grammar rules
+        self.grammar_rules = [
+            # Production 0
+            ProductionRule('PROG', ['MAIN', 'GLOBVARS', 'ALGO', 'FUNCTIONS']),
+            
+            # Production 1 (epsilon production)
+            ProductionRule('GLOBVARS', []),
+            
+            # Production 2
+            ProductionRule('GLOBVARS', ['VTYP', 'VNAME', 'COMMA', 'GLOBVARS']),
+            
+            # Production 3
+            ProductionRule('VTYP', ['NUM_TYPE']),
+            
+            # Production 4
+            ProductionRule('VTYP', ['TEXT_TYPE']),
+            
+            # Production 5
+            ProductionRule('VNAME', ['VNAME_ID']),
+            
+            # Production 6
+            ProductionRule('ALGO', ['BEGIN', 'INSTRUC', 'END']),
+            
+            # Production 7 (epsilon production)
+            ProductionRule('INSTRUC', []),
+            
+            # Production 8
+            ProductionRule('INSTRUC', ['COMMAND', 'SEMICOLON', 'INSTRUC']),
+            
+            # Production 9
+            ProductionRule('COMMAND', ['SKIP']),
+            
+            # Production 10
+            ProductionRule('COMMAND', ['HALT']),
+            
+            # Production 11
+            ProductionRule('COMMAND', ['PRINT', 'ATOMIC']),
+            
+            # Production 12
+            ProductionRule('COMMAND', ['ASSIGN']),
+            
+            # Production 13
+            ProductionRule('COMMAND', ['CALL']),
+            
+            # Production 14
+            ProductionRule('COMMAND', ['BRANCH']),
+            
+            # Production 15
+            ProductionRule('COMMAND', ['RETURN', 'ATOMIC']),
+            
+            # Production 16
+            ProductionRule('ATOMIC', ['VNAME']),
+            
+            # Production 17
+            ProductionRule('ATOMIC', ['CONST']),
+            
+            # Production 18
+            ProductionRule('CONST', ['CONST_N']),
+            
+            # Production 19
+            ProductionRule('CONST', ['CONST_T']),
+            
+            # Production 20
+            ProductionRule('ASSIGN', ['VNAME', 'INPUT_OP', 'INPUT']),
+            
+            # Production 21
+            ProductionRule('ASSIGN', ['VNAME', 'ASSIGN', 'TERM']),
+            
+            # Production 22
+            ProductionRule('CALL', ['FNAME', 'LPAREN', 'ATOMIC', 'COMMA', 'ATOMIC', 'COMMA', 'ATOMIC', 'RPAREN']),
+            
+            # Production 23
+            ProductionRule('BRANCH', ['IF', 'COND', 'THEN', 'ALGO', 'ELSE', 'ALGO']),
+            
+            # Production 24
+            ProductionRule('TERM', ['ATOMIC']),
+            
+            # Production 25
+            ProductionRule('TERM', ['CALL']),
+            
+            # Production 26
+            ProductionRule('TERM', ['OP']),
+            
+            # Production 27
+            ProductionRule('OP', ['UNOP', 'LPAREN', 'ARG', 'RPAREN']),
+            
+            # Production 28
+            ProductionRule('OP', ['BINOP', 'LPAREN', 'ARG', 'COMMA', 'ARG', 'RPAREN']),
+            
+            # Production 29
+            ProductionRule('ARG', ['ATOMIC']),
+            
+            # Production 30
+            ProductionRule('ARG', ['OP']),
+            
+            # Production 31
+            ProductionRule('COND', ['SIMPLE']),
+            
+            # Production 32
+            ProductionRule('COND', ['COMPOSIT']),
+            
+            # Production 33
+            ProductionRule('SIMPLE', ['BINOP', 'LPAREN', 'ATOMIC', 'COMMA', 'ATOMIC', 'RPAREN']),
+            
+            # Production 34
+            ProductionRule('COMPOSIT', ['BINOP', 'LPAREN', 'SIMPLE', 'COMMA', 'SIMPLE', 'RPAREN']),
+            
+            # Production 35
+            ProductionRule('COMPOSIT', ['UNOP', 'LPAREN', 'SIMPLE', 'RPAREN']),
+            
+            # Production 36
+            ProductionRule('UNOP', ['NOT']),
+            
+            # Production 37
+            ProductionRule('UNOP', ['SQRT']),
+            
+            # Production 38
+            ProductionRule('BINOP', ['OR']),
+            
+            # Production 39
+            ProductionRule('BINOP', ['AND']),
+            
+            # Production 40
+            ProductionRule('BINOP', ['EQ']),
+            
+            # Production 41
+            ProductionRule('BINOP', ['GRT']),
+            
+            # Production 42
+            ProductionRule('BINOP', ['ADD']),
+            
+            # Production 43
+            ProductionRule('BINOP', ['SUB']),
+            
+            # Production 44
+            ProductionRule('BINOP', ['MUL']),
+            
+            # Production 45
+            ProductionRule('BINOP', ['DIV']),
+            
+            # Production 46
+            ProductionRule('FNAME', ['FNAME_ID']),
+            
+            # Production 47 (epsilon production)
+            ProductionRule('FUNCTIONS', []),
+            
+            # Production 48
+            ProductionRule('FUNCTIONS', ['DECL', 'FUNCTIONS']),
+            
+            # Production 49
+            ProductionRule('DECL', ['HEADER', 'BODY']),
+            
+            # Production 50
+            ProductionRule('HEADER', ['FTYP', 'FNAME', 'LPAREN', 'VNAME', 'COMMA', 'VNAME', 'COMMA', 'VNAME', 'RPAREN']),
+            
+            # Production 51
+            ProductionRule('FTYP', ['NUM_TYPE']),
+            
+            # Production 52
+            ProductionRule('FTYP', ['VOID']),
+            
+            # Production 53
+            ProductionRule('BODY', ['PROLOG', 'LOCVARS', 'ALGO', 'EPILOG', 'SUBFUNCS', 'END']),
+            
+            # Production 54
+            ProductionRule('PROLOG', ['LBRACE']),
+            
+            # Production 55
+            ProductionRule('EPILOG', ['RBRACE']),
+            
+            # Production 56
+            ProductionRule('LOCVARS', ['VTYP', 'VNAME', 'COMMA', 'VTYP', 'VNAME', 'COMMA', 'VTYP', 'VNAME', 'COMMA']),
+            
+            # Production 57
+            ProductionRule('SUBFUNCS', ['FUNCTIONS']),         
+        ]
 
 
     '''
